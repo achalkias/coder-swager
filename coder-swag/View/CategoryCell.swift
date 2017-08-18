ce95c5cd8d81674b2844e9ea73c037b82a715c5d
Apostolos Chalkias <achalkias@hotmail.com>
2017-08-18 15:14:35 +0300

    Capital letters fix

--- coder-swag/View/CategoryCell.swift
+++ coder-swag/View/CategoryCell.swift
@@ -15,7 +15,7 @@ class CategoryCell: UITableViewCell {
 
     func updateViews(category: Category) {
         categoryImage.image = UIImage(named: category.imageName)
-        categoryTitle.text = category.title
+        categoryTitle.text = category.title.capitalized
     }
 
     
