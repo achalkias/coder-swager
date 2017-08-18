73eaab2b0ca62e35609bf1848d790f15955b1350
Apostolos Chalkias <achalkias@hotmail.com>
2017-08-18 14:31:29 +0300

    Project finished

--- coder-swag/View/CategoryCell.swift
+++ coder-swag/View/CategoryCell.swift
@@ -15,7 +15,7 @@ class CategoryCell: UITableViewCell {
 
     func updateViews(category: Category) {
         categoryImage.image = UIImage(named: category.imageName)
-        categoryTitle.text = category.title.capitalized
+        categoryTitle.text = category.title
     }
 
     
