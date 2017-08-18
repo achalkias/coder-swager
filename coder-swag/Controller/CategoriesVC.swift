b067aea369f1e4653e742d2203a8db29c46f45fb
Apostolos Chalkias <achalkias@hotmail.com>
2017-08-18 16:27:39 +0300

    CVC changes

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -11,7 +11,7 @@ import UIKit
 class CategoriesVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
    
   
-    
+    //This is the category vc
     @IBOutlet weak var categoryTable: UITableView!
 
     override func viewDidLoad() {
